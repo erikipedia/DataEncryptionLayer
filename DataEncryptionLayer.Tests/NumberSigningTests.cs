d6a747eade7dfbe74de6a56a667061c42f4c7c23
erikipedia <erikipedia@gmail.com>
2025-04-15 17:53:25 -0600

    Remove Unused Directives

---
+++ DataEncryptionLayer.Tests/NumberSigningTests.cs
@@ -0,0 +1,16 @@
+﻿namespace DataEncryptionLayer.Tests;
+
+[TestFixture]
+[TestOf(typeof(NumberSigning))]
+public class NumberSigningTests
+{
+    [Test]
+    public void TestLuhnAlgorithm()
+    {
+        string number = "412345678901234";
+        string checkDigit = NumberSigning.ComputeCheckDigit(number);
+        Assert.That(checkDigit, Is.EqualTo("9"));
+        bool checkNumber = NumberSigning.CheckNumber("4123456789012349");
+        Assert.That(checkNumber, Is.True);
+    }
+}
--- DataEncryptionLayer.Tests/TextCryptographyTests.cs
+++ DataEncryptionLayer.Tests/TextCryptographyTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Security.Cryptography;
-using DataEncryptionLayer;
 
 namespace DataEncryptionLayer.Tests;
 
