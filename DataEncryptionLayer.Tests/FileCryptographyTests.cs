a01ddbcc1f08ad8176187fc7daf22bedd9e27565
erikipedia <erikipedia@gmail.com>
2025-04-15 18:50:24 -0600

    Remove DotSettings.user, update .gitignore

---
+++ DataEncryptionLayer.Tests/FileCryptographyTests.cs
@@ -0,0 +1,30 @@
+﻿using DataEncryptionLayer;
+
+namespace DataEncryptionLayer.Tests;
+
+[TestFixture]
+[TestOf(typeof(FileCryptography))]
+public class FileCryptographyTests
+{
+    #region Setup/Teardown
+    
+    [SetUp]
+    public void SetUp()
+    {
+        
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        
+    }
+    
+    #endregion
+    
+    [Test]
+    public void TestFileEncryptDecrypt()
+    {
+        
+    }
+}
--- DataEncryptionLayer/CryptoStreamReader.cs
+++ DataEncryptionLayer/CryptoStreamReader.cs
@@ -4,7 +4,7 @@ namespace DataEncryptionLayer;
 
 /// <summary>
 /// This class encapsulates a workaround for a design flaw in the CryptoStream class,
-/// which results in an exception when closing a stream that has only been partial read.
+/// which results in an exception when closing a stream that has only been partially read.
 /// The workaround suggested by the owner of that class at Microsoft (Shawn Farkas) is to 
 /// simply catch the exception and ignore it.
 /// 
